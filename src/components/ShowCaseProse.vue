20ba0c555e18850986d8e1b3db745b32cb66eae1
M-Shrief <m.shrief.dev@proton.me>
2023-08-10 11:52:35 +0300

    added ShowCaseProse to index page, and added proses store.

---
+++ src/components/ShowCaseProse.vue
@@ -0,0 +1,156 @@
+<template>
+  <section id="proses">
+    <h2 class="title">مختارات نثرية</h2>
+    <div :class="routeName == 'poet' ? 'grid-poet' : ''">
+      <div v-for="prose in proses" :key="prose.id" class="prose-item">
+        <p class="qoute">{{ prose.qoute }}</p>
+        <div class="justify">
+          <a v-if="!(routeName === 'poet')"
+            :href="'/poet/' + prose.poet.id" class="details">{{
+              prose.poet.name
+            }}</a>
+          <button @click="addPrint({ id: prose.id, qoute: prose.qoute })"
+            class="print-button">
+            اضف للطباعة
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+// Stores
+import {appStore} from '../stores/index'
+import {usePrintsStore} from '../stores/prints'
+// types
+import type { Prose, Print } from '../stores/__types__';
+
+defineProps({
+  proses: {
+    type: Array<Prose>,
+    required: true,
+  },
+  routeName: {
+    type: String,
+    required: false
+  }
+});
+
+const printsStore = usePrintsStore(appStore);
+function addPrint(print: Print) {
+  return printsStore.addPrint(print);
+}
+
+// defineEmits(['print']);
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/mixins.scss';
+
+$mainColor: var(--text1);
+$secondaryColor: var(--surface3);
+
+.title {
+  text-align: center;
+  background-color: $secondaryColor;
+  color: $mainColor;
+  padding: 0.3rem;
+  margin: 1rem 0.3rem 0;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
+  border-radius: 1.5rem;
+
+  @include mQ($breakpoint-lg) {
+    padding: 0.3rem;
+    margin: 0.6rem 0.4rem 0;
+    font-size: 1.3rem;
+  }
+
+  @include mQ($breakpoint-md) {
+    padding: 0.2rem;
+    margin: 0.5rem 0.3rem 0;
+    font-size: 1.2rem;
+  }
+
+  @include mQ($breakpoint-sm) {
+    padding: 0.1rem;
+    margin: 0.3rem 0.1rem 0;
+    font-size: 1rem;
+  }
+}
+
+.grid-poet {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  @include mQ($breakpoint-sm) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+.prose-item {
+  position: relative;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
+  padding: 0.3rem;
+  margin: 0.5rem;
+  border-radius: 1.5rem;
+  background: $secondaryColor;
+  color: $mainColor;
+
+  @include mQ($breakpoint-md) {
+    padding: 0.2rem;
+    margin: 0.3rem;
+  }
+
+  @include mQ($breakpoint-sm) {
+    padding: 0.1rem;
+    margin: 0.2rem;
+  }
+
+  .qoute {
+    text-align: center;
+    font-size: 1.2rem;
+    padding-bottom: 0.8rem;
+
+    @include mQ($breakpoint-lg) {
+      font-size: 1.1rem;
+    }
+
+    @include mQ($breakpoint-md) {
+      font-size: 1rem;
+    }
+
+    @include mQ($breakpoint-sm) {
+      font-size: 0.8rem;
+    }
+  }
+
+  .details {
+    text-decoration: none;
+    color: $mainColor;
+    position: absolute;
+    bottom: 0.7rem;
+    right: 0.7rem;
+
+    &:hover {
+      font-weight: 600;
+    }
+
+    @include mQ($breakpoint-lg) {
+      font-size: 0.7rem;
+    }
+
+    @include mQ($breakpoint-md) {
+      font-size: 0.7rem;
+    }
+
+    @include mQ($breakpoint-sm) {
+      font-size: 0.6rem;
+    }
+  }
+
+  .print-button {
+    @include add-print-btn;
+  }
+}
+</style>
---
+++ src/stores/proses.ts
@@ -0,0 +1,42 @@
+import {ref, computed} from 'vue';
+import { defineStore } from 'pinia';
+import { AxiosError } from 'axios';
+import {baseHttp} from '../utils/axios'
+// types
+import type { Prose } from './__types__';
+// Composables
+// import { useAxiosError } from '../composables/error';
+
+export const useProseStore = defineStore('proses', () => {
+  const proses = ref<Prose[]>([]);
+  const getProses = computed<Prose[]>(() => proses.value);
+  async function fetchProses() {
+    try {
+      const req = await baseHttp.get(`/proses`);
+      proses.value = req.data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        // useAxiosError(error);
+        return;
+      }
+      alert(error);
+    }
+  };
+
+  const randomProses = ref<Prose[]>([]);
+  const getRandomProses = computed<Prose[]>(() => randomProses.value);
+  async function fetchRandomProses(num: number) {
+    try {
+      const req = await baseHttp.get(`/proses/random?num=${num}`);
+      randomProses.value = req.data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        // useAxiosError(error);
+        return;
+      }
+      alert(error);
+    }
+  };
+
+  return {getProses, fetchProses,  getRandomProses, fetchRandomProses};
+});
