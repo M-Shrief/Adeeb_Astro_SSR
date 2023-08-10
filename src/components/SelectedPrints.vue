1b96b1e7479fe471d8ae2ee04deb3ff99fe2e07f
M-Shrief <m.shrief.dev@proton.me>
2023-08-10 11:51:52 +0300

    added ShowCasePoetry & SelectedPrints components to index page, and added chosenVerses & prints stores.

---
+++ src/components/SelectedPrints.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="selected-prints" dir="rtl">
+    <p><a :href="isPartner ? '/partners/ordering' : '/ordering'"
+        class="title">للطباعة</a> </p>
+    <ul class="prints">
+      <li class="prints-item" v-for="print in getPrints" :key="print.id"
+        @dblclick="removePrint(print)">
+        <!-- poetry -->
+        <p v-if="print.verses">{{ print.verses[0].first }}..</p>
+        <!-- prose -->
+        <p v-else-if="print.qoute">{{ print.qoute.slice(0, 15) }}..</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {computed} from 'vue'
+// Stores
+import {appStore} from '../stores/index'
+import {usePrintsStore} from '../stores/prints'
+// types
+import type { Print } from '../stores/__types__';
+
+
+const printsStore = usePrintsStore(appStore);
+const getPrints = computed(() => {
+  return printsStore.getPrints;
+});
+
+function removePrint(print: Print) {
+  return printsStore.removePrint(print);
+}
+
+defineProps({
+  isPartner: {
+    type: Boolean,
+    required: true
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/mixins.scss';
+
+$mainColor: var(--text1);
+$secondaryColor: var(--surface4);
+
+#selected-prints {
+  position: fixed;
+  bottom: 0.3rem;
+  right: 0.1rem;
+  display: flex;
+  flex-direction: row;
+  background: $secondaryColor;
+  padding: 0.3rem 0.7rem;
+  border-radius: 2rem;
+
+  @include mQ($breakpoint-lg) {
+    padding: 0.2rem 0.5rem;
+  }
+
+  @include mQ($breakpoint-md) {
+    padding: 0.2rem 0.4rem;
+  }
+
+  @include mQ($breakpoint-sm) {
+    padding: 0.1rem 0.3rem;
+  }
+
+  .title {
+    margin-left: 0.3rem;
+    font-weight: 600;
+    color: $mainColor;
+    opacity: 85%;
+    text-decoration: none;
+
+    &:hover {
+      opacity: 100%;
+    }
+
+    @include mQ($breakpoint-lg) {
+      margin-left: 0.3rem;
+      font-size: 1rem;
+    }
+
+    @include mQ($breakpoint-md) {
+      margin-left: 0.2rem;
+      font-size: 0.9rem;
+    }
+
+    @include mQ($breakpoint-sm) {
+      margin-left: 0.1rem;
+      font-size: 0.8rem;
+    }
+  }
+
+  .prints {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+
+    .prints-item {
+      padding: 0.2rem;
+      margin-left: 0.3rem;
+      // background: rgba($color: $mainColor, $alpha: 0.8);
+      background: $mainColor;
+      color: $secondaryColor;
+      cursor: pointer;
+
+      p {
+        display: none;
+        padding: 0.25rem;
+      }
+
+      &:hover {
+        p {
+          display: inline;
+          padding: 0rem;
+          font-weight: 600;
+        }
+      }
+
+      @include mQ($breakpoint-sm) {
+        padding: 0.1rem;
+
+        &:hover p {
+          font-size: 0.8rem;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/stores/chosenVerses.ts
@@ -0,0 +1,42 @@
+import {ref, computed} from 'vue';
+import { defineStore } from 'pinia';
+import { AxiosError } from 'axios';
+import {baseHttp} from '../utils/axios'
+// types
+import type { ChosenVerse } from './__types__';
+// Composables
+// import { useAxiosError } from '../composables/error';
+
+export const useChosenVerseStore = defineStore('chosenVerses', () => {
+  const chosenVerses =  ref<ChosenVerse[]>([]);
+  const getChosenVerses = computed<ChosenVerse[]>(() => chosenVerses.value);
+  async function fetchChosenVerses() {
+    try {
+      const req = await baseHttp.get(`/chosenverses`);
+      chosenVerses.value = req.data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        // useAxiosError(error);
+        return;
+      }
+      alert(error);
+    }
+  };
+
+  const randomChosenVerses =ref<ChosenVerse[]>([]);
+  const getRandomChosenVerses = computed<ChosenVerse[]>(() => randomChosenVerses.value);
+  async function fetchRandomChosenVerses(num: number) {
+    try {
+      const req = await baseHttp.get(`/chosenverses/random?num=${num}`);
+      randomChosenVerses.value = req.data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        // useAxiosError(error);
+        return;
+      }
+      alert(error);
+    }
+  };
+
+    return {getChosenVerses, fetchChosenVerses, getRandomChosenVerses, fetchRandomChosenVerses}
+});
