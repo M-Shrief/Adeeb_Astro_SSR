0cd027c8c0e7f56e0b8b23967e835c8affb1ae49
M-Shrief <m.shrief.dev@proton.me>
2023-08-14 15:08:16 +0300

    added Ordering page for guests with orderForm component that uses vee-validate & Yup.

---
+++ src/components/OrderForm.vue
@@ -0,0 +1,259 @@
+<template>
+  <Form @submit="onSubmit" dir="rtl">
+    <div id="confirmation">
+      <div id="customer-details" v-if="partner">
+        <div class="container">
+          <p>الاسم: {{ partner.name }}</p>
+        </div>
+        <div class="container">
+          <p>الهاتف: {{ partner.phone }}</p>
+        </div>
+        <div class="container">
+          <label for="address">العنوان: </label>
+          <Field name="address" id="address" autocomplete="address"  :rules="addressRules" />
+          <ErrorMessage name="address" class="error" />
+        </div>
+      </div>
+      <div id="customer-details" v-else>
+        <div class="container">
+          <label for="name">الاسم: </label>
+          <Field name="name" id="name" autocomplete="name"  :rules="nameRules" />
+          <ErrorMessage name="name" class="error" />
+        </div>
+        <div class="container">
+          <label for="phone">الهاتف: </label>
+          <Field name="phone" id="phone" autocomplete="phone" :rules="phoneRules" />
+          <ErrorMessage name="phone" class="error" />
+        </div>
+        <div class="container">
+          <label for="address">العنوان: </label>
+          <Field name="address" id="address" autocomplete="address" :rules="addressRules" />
+          <ErrorMessage name="address" class="error" />
+        </div>
+      </div>
+      <div class="errors">
+      </div>
+
+
+      <div id="products">
+        <div v-for="product, index in products" class="product-details"
+          :key="index"
+          :style="{ color: product.fontColor, background: product.backgroundColor }"
+          @dblclick="deleteProduct(products as Product[], product)">
+          <p>{{ product.fontType }}</p>
+          <p v-if="product.print.verses"> {{ product.print.verses[0].first
+          }}...</p>
+          <p v-else-if="product.print.qoute">
+            {{ product.print.qoute.slice(0, 30) }}...</p>
+        </div>
+        <div v-for="productGroup, index in productGroups" :key="index">
+          <div class="group-details"
+            :style="{ color: productGroup.fontColor, background: productGroup.backgroundColor }">
+            <p>نوع الخط: {{ productGroup.fontType }} </p>
+            <p>{{ productGroup.prints.length }} طبعات</p>
+          </div>
+          <div class="group-prints">
+            <div v-for="print, index in productGroup.prints" :key="index"
+              class="group-print"
+              :style="{ color: productGroup.fontColor, background: productGroup.backgroundColor }"
+              @dblclick="deleteFromProductGroup(productGroup, print)">
+              <p v-if="print.verses"> {{ print.verses[0].first }}...</p>
+              <p v-else-if="print.qoute"> {{ print.qoute.slice(0, 30) }}...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button type="submit">تأكيد الطلب</button>
+  </Form>
+</template>
+
+<script lang="ts" setup>
+// Validation
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { nameRules, phoneRules, addressRules } from '../utils/forms.schema'
+// types
+import type { Product, Print, ProductGroup, Partner, Order } from '../stores/__types__';
+
+const props = defineProps<{
+  products?:  Array<Product>,
+  partner?: Partner | null,
+  productGroups?: Array<ProductGroup>,
+}>();
+
+
+function deleteProduct(products: Product[], product: Product) {
+  let productIndex = products.map(product => product.print.id).indexOf(product.print.id);
+  products.splice(productIndex, 1);
+}
+
+function deletePrint(productGroup: ProductGroup, print: Print) {
+  let printIndex = productGroup.prints.map((print: Print) => print.id).indexOf(print.id);
+  productGroup.prints.splice(printIndex, 1);
+}
+
+// ProductGroup
+function deleteFromProductGroup(productGroup: ProductGroup, print: Print) {
+  if (props.productGroups && productGroup.prints.length == 1) {
+    let productGroupIndex = props.productGroups.map(productGroup => productGroup.prints.length).indexOf(1);
+    props.productGroups.splice(productGroupIndex, 1);
+  } else {
+    deletePrint(productGroup, print)
+  }
+}
+
+const emits = defineEmits(['partnerOrder', 'guestOrder']);
+
+async function onSubmit(values: any) {
+  let order: Order;
+  if (props.partner && props.productGroups) {
+    order = {
+      partner: props.partner.id,
+      name: props.partner.name,
+      phone: props.partner.phone,
+      ...values,
+      products: props.productGroups
+    };
+    emits('partnerOrder', order);
+  } else {
+    order = {
+      ...values,
+      products: props.products
+    };
+    emits('guestOrder', order);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/mixins.scss';
+
+$mainColor: var(--text1);
+$secondaryColor: var(--surface2);
+
+form {
+  background-color: $secondaryColor;
+  margin: 1rem;
+  border-radius: 1.5rem;
+  padding: 0.5rem;
+
+  #customer-details {
+    color: $mainColor;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    .container {
+      font-size: 1.1rem;
+      padding: 0.8rem;
+      margin-right: 0.2rem;
+      margin-top: 0.4rem;
+
+      #name,
+      #phone,
+      #address {
+        background: $mainColor;
+        box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
+        border: none;
+        border-radius: 1rem;
+        color: $secondaryColor;
+      }
+
+      #address {
+        color: $secondaryColor;
+        background-color: $mainColor;
+        border: 1px solid $secondaryColor;
+      }
+
+      .error {
+        color: #c80815;
+        margin-right: 1rem;
+      }
+    }
+
+    @include mQ($breakpoint-sm) {
+      padding: 0.1rem;
+      margin-top: 0.1rem;
+
+      #name,
+      #phone,
+      #address {
+        font-size: 0.8rem;
+      }
+    }
+
+    @include mQ($breakpoint-md) {
+      padding: 0.3rem;
+      margin-top: 0.3rem;
+
+      #name,
+      #phone,
+      #address {
+        font-size: 0.7rem;
+      }
+    }
+  }
+
+
+  #products {
+    font-weight: 550;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    justify-content: space-around;
+
+    .product-details,
+    .group-details,
+    .group-print {
+      position: relative;
+      display: flex;
+      justify-content: space-around;
+      flex-direction: row;
+      padding: 0.2rem;
+      margin: 0.3rem .5rem;
+      border-radius: 1.5rem;
+      border: 1px solid $mainColor;
+
+      p {
+        margin: 0 0.6rem;
+      }
+
+      @include mQ($breakpoint-md) {
+        padding: 0.15rem;
+        margin: 0.15rem;
+
+        p {
+          font-size: 0.8rem;
+        }
+      }
+
+      @include mQ($breakpoint-sm) {
+        padding: 0.1rem;
+        margin: 0.1rem;
+
+        p {
+          font-size: 0.7rem;
+        }
+      }
+    }
+
+    .product-details,
+    .group-print {
+      cursor: pointer;
+
+    }
+
+    .group-prints {
+      font-size: 0.75rem;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  button[type='submit'] {
+    position: relative;
+    right: 45%;
+    margin: 0.5rem auto;
+    @include submit-btn
+  }
+}
+</style>
---
+++ src/components/pages/GuestOrdering.vue
@@ -0,0 +1,239 @@
+<template>
+  <main id="printing">
+    <div class="container">
+      <section id="preview">
+        <h3>اختر من الاشعار للطباعة</h3>
+        <PrintCustomization :colors="colors"
+          @fontColor="(color: string) => fontColor = color"
+          @backgroundColor="(color: string) => backgroundColor = color" />
+        <div v-if="preview.verses" v-for="verse, index in preview.verses"
+          :key="index" class="verse" id="print"
+          :style="{ color: fontColor, background: backgroundColor }">
+          <p>{{ verse.first }}</p>
+          <p dir="ltr">{{ verse.sec }}</p>
+        </div>
+        <div v-else-if="preview.qoute" class="qoute" id="print"
+          :style="{ color: fontColor, background: backgroundColor }">
+          <p>{{ preview.qoute }}</p>
+        </div>
+        <button @click="addProduct(preview, [fontColor, backgroundColor])">اضافة
+          الطلب</button>
+        <div>
+        </div>
+      </section>
+      <OrderForm :products="getProducts" @guest-order="(order: Order) => confirmGuestOrder(order)"/>
+    </div>
+
+    <section id="prints">
+      <h3>المختار للطباعة</h3>
+      <div id="randoms">
+        <div id="buttons">
+          <button @click="getRandomPoetry(1)">شعر عشوائي</button>
+          <button @click="getRandomProse(1)">نثر عشوائي</button>
+        </div>
+        <div v-if="randomPrint">
+          <div @click="preview = randomPrint">
+            <div v-if="randomPrint.verses" v-for="verse,index in randomPrint.verses"
+              :key="index" class="verse">
+              <p>{{ verse.first }}</p>
+              <p dir="ltr">{{ verse.sec }}</p>
+            </div>
+            <div v-else-if="randomPrint.qoute" class="qoute">
+              <p>{{ randomPrint.qoute }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ShowCasePrints :prints="getPrints" @remove="(print) => removePrint(print)"
+        @print="(print: Print) => preview = print" />
+    </section>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+// stores
+import { getPrints, removePrint } from "../../stores/prints";
+import { $randomChosenVerses, fetchRandomChosenVerses } from "../../stores/chosenVerses";
+import { $randomProses, fetchRandomProses } from "../../stores/proses";
+import { colors, newOrder, reset, getProducts, addProduct } from '../../stores/orders';
+// components
+import PrintCustomization from '../PrintCustomization.vue';
+import OrderForm from "../OrderForm.vue";
+import ShowCasePrints from '../ShowCasePrints.vue';
+// Types
+import type { Order,Prose, ChosenVerse, Print } from '../../stores/__types__';
+
+let preview = ref({} as Print);
+let randomPrint = ref();
+
+let fontColor = ref(colors[0]);
+let backgroundColor = ref(colors[1]);
+
+const confirmGuestOrder = async (order: Order) => {
+  await newOrder(order);
+  reset();
+  window.location.href = '/history/';
+}
+
+function getRandomPoetry(num: number) {
+  fetchRandomChosenVerses(num);
+  randomPrint.value = ($randomChosenVerses.get() as ChosenVerse[])[0];
+}
+
+function getRandomProse(num: number) {
+  fetchRandomProses(num);
+  randomPrint.value = ($randomProses.get() as Prose[])[0];
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/mixins.scss';
+$mainColor: var(--text1);
+$secondaryColor: var(--surface2);
+
+#printing {
+  display: grid;
+  grid-template-columns: 75% 25%;
+  color: $mainColor;
+  margin-top: 1rem;
+
+  @include mQ($breakpoint-md) {
+    grid-template-columns: 70% 30%;
+  }
+
+  h3 {
+    text-align: center;
+
+    @include mQ($breakpoint-md) {
+      font-size: 1.1rem;
+    }
+
+    @include mQ($breakpoint-sm) {
+      font-size: 1rem;
+    }
+  }
+
+  .container {
+    min-height: 50vh;
+  }
+
+  #preview {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    margin: 1rem;
+    border-radius: 1.5rem;
+    background: $secondaryColor;
+
+    .verse,
+    .qoute {
+      padding: 0 0.6rem;
+      font-size: 1.6rem;
+      width: 70%;
+      margin: 0 auto;
+
+      @include mQ($breakpoint-md) {
+        font-size: 0.9rem;
+      }
+
+      @include mQ($breakpoint-sm) {
+        font-size: 0.8rem;
+      }
+    }
+
+    .qoute {
+      text-align: center;
+    }
+
+    #customization {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      padding: 0.3rem;
+
+      .select {
+        display: flex;
+        flex-direction: column;
+        padding: 0.5rem;
+
+        select {
+          border: none;
+        }
+
+        #fontType {
+          margin-top: 0.3rem;
+        }
+      }
+
+      @include mQ($breakpoint-md) {
+        .select {
+          padding: 0.3rem;
+        }
+
+        label {
+          font-size: 0.9rem;
+        }
+      }
+
+      @include mQ($breakpoint-sm) {
+        .select {
+          padding: 0.1rem;
+        }
+
+        label {
+          font-size: 0.6rem;
+        }
+      }
+    }
+
+    button {
+      margin: 0.5rem auto;
+      @include submit-btn
+    }
+
+  }
+
+  #prints {
+    background: $secondaryColor;
+    margin-left: 0.5rem;
+    border-radius: 1rem;
+
+    #randoms {
+      color: $mainColor;
+      border: 1px solid $mainColor;
+      list-style: none;
+      border-radius: 5px;
+      padding: 0.5rem;
+      margin: 0.5rem;
+
+      .qoute {
+        text-align: center;
+      }
+
+      #buttons {
+        display: flex;
+        justify-content: space-around;
+
+        button {
+          background: $mainColor;
+          color: $secondaryColor;
+          padding: 0.4rem;
+          border: none;
+          border-radius: 0.7rem;
+          cursor: pointer;
+
+          @include mQ($breakpoint-md) {
+            padding: 0.15rem;
+          }
+
+          @include mQ($breakpoint-sm) {
+            padding: 0.1rem;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
