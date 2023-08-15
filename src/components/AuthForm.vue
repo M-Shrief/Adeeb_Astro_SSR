0513c02757395eddf42a0981602ba8bf6a17b087
M-Shrief <m.shrief.dev@proton.me>
2023-08-15 15:38:30 +0300

    added Partner's auth page with AuthForm component, and added partners' store and js-cookie package.

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,127 @@
+<template>
+  <section id="authentication">
+    <h3>{{ isRegistered ? "تسجيل الدخول" : "تسجيل حساب جديد" }} </h3>
+    <Form @submit="onSubmit" id="auth-form">
+      <div v-if="!isRegistered" class="input-cont">
+        <label for="name">الاسم: </label>
+        <Field name="name" id="name" autocomplete="name"  :rules="nameRules" />
+        <ErrorMessage name="name" class="error" />
+      </div>
+      <div class="input-cont">
+        <label for="phone">رقم الهاتف: </label>
+        <Field name="phone" id="phone" autocomplete="phone"  :rules="phoneRules" />
+        <ErrorMessage name="phone" class="error" />
+      </div>
+      <div class="input-cont">
+        <label for="password">كلمة السر: </label>
+        <Field type="password" name="password" id="password" 
+          :rules="passwordRules" />
+        <ErrorMessage name="password" class="error" />
+      </div>
+      <button type="submit">التأكيد</button>
+    </Form>
+    <button id="toggle" @click="isRegistered = !isRegistered">
+      {{ !isRegistered ? "تسجيل الدخول" : "تسجيل حساب جديد" }}
+    </button>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+// Validation
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { nameRules, phoneRules, passwordRules } from '../utils/forms.schema'
+// stores
+import { isPartner, login, signup } from '../stores/partners';
+// Composables
+import { useAxiosError } from '../composables/errorsNotifications';
+
+const isRegistered = ref(true);
+
+// const emits = defineEmits(['login', 'signup'])
+async function onSubmit(values: any) {
+  if (isRegistered.value) {
+    // emits('login', values)
+    await login(values)
+    .then(() => {
+      if (isPartner.value) window.location.href = '/';
+    })
+    .catch(error => useAxiosError(error));
+  } else {
+    // emits('signup', values)
+    await signup(values)
+    .then(() => {
+      if (isPartner.value) window.location.href = '/';
+    })
+    .catch(error => useAxiosError(error));
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$mainColor: var(--text1);
+$secondaryColor: var(--surface2);
+
+#authentication {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 40vh;
+  margin: 1rem;
+  padding: 4rem;
+  background-color: $secondaryColor;
+  border-radius: 1rem;
+  color: $mainColor;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+
+  h3 {
+    text-align: center;
+  }
+
+  #auth-form {
+    display: flex;
+    flex-direction: column;
+  }
+  .input-cont {
+    display: block;
+    padding: 0.2rem;
+    margin: 0.3rem;
+    input {
+      color: $secondaryColor;
+      background: $mainColor;
+      box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
+      border: none;
+      border-radius: 8px;
+      min-width: 30%;
+    }
+  }
+
+  .error {
+    color: #c80815;
+    margin-right: 1rem;
+  }
+
+  button {
+    background-color: $mainColor;
+    color: $secondaryColor;
+    border: none;
+    border-radius: 1rem;
+    padding: 0.5rem;
+    cursor: pointer;
+    font-size: 1.2rem;
+  }
+
+
+  button[type='submit'] {
+    place-self: center;
+  }
+
+  #toggle {
+    position: absolute;
+    left: 1rem;
+    bottom: 1rem;
+  }
+}
+</style>
+
---
+++ src/stores/partners.ts
@@ -0,0 +1,71 @@
+import {ref, computed} from 'vue';
+import  { AxiosError } from 'axios';
+import Cookies, {type CookieAttributes} from 'js-cookie';
+// Utils
+import {baseHttp} from '../utils/axios'
+// Stores
+import { reset } from './orders';
+// types
+import type { Partner } from './__types__';
+
+// Composables
+import { useAxiosError } from '../composables/errorsNotifications';
+import { useSessionStorage } from '@vueuse/core';
+
+const partner =  ref(useSessionStorage< string | null>('partner', null as string | null));
+
+export const getPartner =  computed<Partner | null>(() => {
+  if(partner && typeof partner.value == 'string') return JSON.parse(partner.value);
+  return null
+});
+
+export const isPartner = computed<boolean>(() => {
+  return getPartner.value ? true : false;
+}); 
+
+const tokenCookieOptions: CookieAttributes = {
+  expires: 1 / 24 / 2,   // default to one day, so we make 1 day / 24 hours = 1 hour then we multiply it by 2
+  sameSite: "strict",
+  httpOnly: false
+}
+
+export async function signup(partnerData: Partner) {
+  try {
+    const req = await baseHttp.post(`/partner/signup`, partnerData);
+    partner.value = JSON.stringify(req.data.partner);    
+    Cookies.set("accessToken", req.data.accessToken, tokenCookieOptions)
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      useAxiosError(error);
+      return;
+    }
+    alert(error);
+  }
+};
+
+export async function login(partnerData: Partner) {
+  try {
+    const req = await baseHttp.post(`/partner/login`, partnerData);
+    partner.value = JSON.stringify(req.data.partner);
+    Cookies.set("accessToken", req.data.accessToken, tokenCookieOptions);
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      useAxiosError(error);
+      return;
+    }
+    alert(error);
+  }
+};
+
+const removeAllCookies = () => {
+  Object.keys(Cookies.get()).forEach(function(cookieName) {
+    Cookies.remove(cookieName);
+  });
+}
+
+export async function logout() {
+  // reset orders value
+  reset();
+  removeAllCookies()
+  partner.value = null;
+};
