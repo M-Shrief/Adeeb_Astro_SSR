41bbb0e2277c7352f8eaf71558a80cc9b530883c
M-Shrief <m.shrief.dev@proton.me>
2023-09-03 23:28:59 +0300

    will try vercel instead of the hussle of using deno deploy for now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-import deno from "@astrojs/deno";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     vue({
-    // appEntrypoint: '/src/pages/_app' 
+      // appEntrypoint: '/src/pages/_app' 
     })
   ],
   server: {
     port: 5173
   },
   output: "hybrid",
-  adapter: deno()
+  adapter: vercel()
 });
