4f336706dbbd1afcc09483a77e2dea6c4987fd17
M-Shrief <m.shrief.dev@proton.me>
2023-09-05 18:31:30 +0300

    trying cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue({
+  integrations: [vue({
     // appEntrypoint: '/src/pages/_app' 
-    })
-  ],
+  })],
   server: {
     port: 5173
   },
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: cloudflare(),
+  // adapter: node({
+  //   mode: "standalone"
+  // }),
   // vite: {
   //   ssr: {
   //     noExternal: ['path-to-regexp'],
