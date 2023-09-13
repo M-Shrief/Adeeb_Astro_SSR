59e60fa2cd678dfdab836468c4d03743ac517350
M-Shrief <m.shrief.dev@proton.me>
2023-09-13 14:09:19 +0300

    added used Astro.url to get active navbar page and to add active class to active links, removed old active-class from a tags.

--- src/components/NavbarRoutes.vue
+++ src/components/NavbarRoutes.vue
@@ -1,24 +1,41 @@
 <template>
     <div class="nav-row-group">
       <a :href="getPartner ? '/partners/ordering' : '/ordering'"
-        class="nav-item" active-class="active">طلباتك</a>
+        class="nav-item" >طلباتك</a>
       <a :href="getPartner ? '/partners/history' : '/history'"
-        class="nav-item" active-class="active">مراجعة
+        class="nav-item" >مراجعة
         الطلبات</a>
       <a :href="'/about'" class="nav-item"
-        active-class="active">قصتنا</a>
+        >قصتنا</a>
 
       <span v-if="getPartner" class="partner-name">{{ getPartner.name }}</span>
       <span v-if="getPartner" class="nav-item" @click="logout">تسجيل
         الخروج</span>
-      <a href="/partners/auth" class="nav-item" active-class="active" v-else >كن
+      <a href="/partners/auth" class="nav-item"  v-else >كن
         شريكاً</a>
     </div>
 </template>
 
 <script lang="ts" setup>
 // Stores
+import { onMounted } from 'vue';
 import { getPartner, logout } from '../stores/partners';
+
+const props = defineProps<{
+  activeUrl: string; 
+}>()
+
+onMounted(() => {
+  let url: string;
+  if(props.activeUrl.endsWith('/')) {
+    url = props.activeUrl.slice(0, props.activeUrl.length - 1);
+  } 
+  else url = props.activeUrl;
+
+  const activeLink = document.querySelector(`a[href*='${url}']`);
+
+  if(activeLink) activeLink.classList.add('active')}
+)
 </script>
 
 <style lang="scss" scoped>
@@ -48,11 +65,14 @@ import { getPartner, logout } from '../stores/partners';
         padding: 0.1rem 0.2rem;
         margin: 0.1rem;
       }
+    }
 
-      &.active {
-        background-color: $mainColor;
+    .active {
+      background-color: $mainColor;
+      color: #2c3e50;
+      border-radius: 1rem;
+      &:hover {
         color: #2c3e50;
-        border-radius: 1rem;
       }
     }
 
