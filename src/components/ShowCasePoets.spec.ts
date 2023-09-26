b915bb76499491a77b619998ecb13c6a0ab8ab88
M-Shrief <m.shrief.dev@proton.me>
2023-09-26 15:53:04 +0300

    configured Vitest to test Vue components, added vitest component testing for ShowCasePoets.vue

---
+++ src/components/ShowCasePoets.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils';
+// Components
+import ShowCasePoets from './ShowCasePoets.vue';
+// Types
+import type { Poet } from '../stores/__types__';
+
+test('It render a link with poetName', () => {
+    const wrapper = mount(ShowCasePoets, {
+        props: {
+            poets: [
+                {
+                  id: 'e7749f21-9cf9-4981-b7a8-2ce262f159f6',
+                  name: 'عنترة بن شداد',
+                  time_period: 'العصر الجاهلي',
+                } as Poet,
+            ],
+        }
+    })
+    expect(wrapper.find('a[href*="/poet/e7749f21-9cf9-4981-b7a8-2ce262f159f6"').exists()).equal(true)
+    expect(wrapper.get('.poet p').text()).equal('عنترة بن شداد')
+})
--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,21 @@
 import { getViteConfig } from 'astro/config';
+import vue from '@vitejs/plugin-vue';
 
 export default getViteConfig({
   define: {
     'import.meta.vitest': 'undefined',
   },
+  plugins: [vue()],
   test: {
+  
     // Vitest configuration options
-    includeSource: ['src/stores/*.{js,ts}'],
+    includeSource: [  
+      'src/stores/*.{js,ts}', // in-source Unit testing 
+      'src/components/*.spec.{js,ts}' // Component testing
+    ],
     globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom'
   },
 });
