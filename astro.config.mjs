1de42c665ee7d8098de359819ed23fa1cc1e642a
M-Shrief <m.shrief.dev@proton.me>
2023-09-27 15:51:10 +0300

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import node from "@astrojs/node";
-
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adeeb.m-shrief.tech',
   integrations: [
     vue({
       // appEntrypoint: '/src/pages/_app' 
     }),
     prefetch({
       selector: ["a[href^='/ordering']", "a[href^='/partners/ordering']"],
-      throttle: 1,
+      throttle: 1
+    }),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      entryLimit: 100
     })
   ],
   server: {
