a407a629d1eedeab76f722da485f2b59ce626bd3
M-Shrief <m.shrief.dev@proton.me>
2023-09-28 21:39:02 +0300

    added component testing with Vitest for PrintCustomization.

---
+++ src/components/PrintCustomization.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it, vi } from 'vitest'
+import { mount } from '@vue/test-utils';
+// Components
+import PrintCustomization from './PrintCustomization.vue';
+
+describe('<PrintCustomization />', () => {
+    it('Contains 8 colors for Fonts & backgound each', () => {
+        const wrapper = mount(PrintCustomization);
+
+        expect(wrapper.findAll('.colors')[0].text()).equal('الخط:')
+        expect(wrapper.findAll('.colors')[1].text()).equal('الخلفية:')
+        expect(wrapper.findAll('.colors > button').length).equal(16)
+    })
+})
