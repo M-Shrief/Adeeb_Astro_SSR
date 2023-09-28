857711ffa0ca92de2b4680c139787cf6d291fdeb
M-Shrief <m.shrief.dev@proton.me>
2023-09-28 18:55:53 +0300

    used describe and improved test description for ShowCasePoets.

--- src/components/ShowCasePoets.spec.ts
+++ src/components/ShowCasePoets.spec.ts
@@ -1,21 +1,24 @@
+import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils';
 // Components
 import ShowCasePoets from './ShowCasePoets.vue';
 // Types
 import type { Poet } from '../stores/__types__';
 
-test('It render a link with poetName', () => {
-    const wrapper = mount(ShowCasePoets, {
-        props: {
-            poets: [
-                {
-                  id: 'e7749f21-9cf9-4981-b7a8-2ce262f159f6',
-                  name: 'عنترة بن شداد',
-                  time_period: 'العصر الجاهلي',
-                } as Poet,
-            ],
-        }
+describe('<ShowCasePoets />', () => {
+    it('It render a link with poetName', () => {
+        const wrapper = mount(ShowCasePoets, {
+            props: {
+                poets: [
+                    {
+                        id: 'e7749f21-9cf9-4981-b7a8-2ce262f159f6',
+                        name: 'عنترة بن شداد',
+                        time_period: 'العصر الجاهلي',
+                    } as Poet,
+                ],
+            }
+        })
+        expect(wrapper.find('a[href*="/poet/e7749f21-9cf9-4981-b7a8-2ce262f159f6"').exists()).equal(true)
+        expect(wrapper.get('.poet p').text()).equal('عنترة بن شداد')
     })
-    expect(wrapper.find('a[href*="/poet/e7749f21-9cf9-4981-b7a8-2ce262f159f6"').exists()).equal(true)
-    expect(wrapper.get('.poet p').text()).equal('عنترة بن شداد')
 })
