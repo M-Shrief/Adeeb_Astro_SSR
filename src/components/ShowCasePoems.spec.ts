cf60ffdc60dbf6164fac3b44220bb149f2ccb03e
M-Shrief <m.shrief.dev@proton.me>
2023-09-28 22:36:49 +0300

    added component testing with Vitest for ShowCasePoems.

---
+++ src/components/ShowCasePoems.spec.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it, vi } from 'vitest'
+import { flushPromises, mount } from '@vue/test-utils';
+import waitForExpect from 'wait-for-expect';
+// Componetns
+import ShowCasePoems from './ShowCasePoems.vue';
+import type { Poem, Poet } from '../stores/__types__';
+
+describe('ShowCasePoems', () => {
+    it('renders on main page, with poet name', () => {
+        const wrapper = mount(ShowCasePoems, {
+            props: {
+                poems: [
+                    {
+                      id: 'e7749f21-9cf9-4981-b7a8-2ce262f159f6',
+                      intro: 'حُكمُ المَنِيَّةِ في البَرِيَّةِ جاري',
+                      poet: {
+                        id: '6371ea89885e286801faccaa',
+                        name: 'التهامي',
+                      },
+                      reviewed: true,
+                    } as Poem
+                ],
+            }
+        })
+
+        expect(wrapper.find('.poem p').text()).toEqual('حُكمُ المَنِيَّةِ في البَرِيَّةِ جاري - التهامي');
+        expect(wrapper.find('.poem').attributes()).toHaveProperty('href', '/poem/e7749f21-9cf9-4981-b7a8-2ce262f159f6');
+    })
+    it('renders on poet page, without poet name', () => {
+        const wrapper = mount(ShowCasePoems, {
+            props: {
+                poems: [
+                    {
+                        id: 'e7749f21-9cf9-4981-b7a8-2ce262f159f6',
+                        intro: 'حُكمُ المَنِيَّةِ في البَرِيَّةِ جاري',
+                        reviewed: true,
+                    } as Poem
+                ],
+            }
+        })
+
+        expect(wrapper.find('.poem p').text()).toEqual('حُكمُ المَنِيَّةِ في البَرِيَّةِ جاري..');
+        expect(wrapper.find('.poem').attributes()).toHaveProperty('href', '/poem/e7749f21-9cf9-4981-b7a8-2ce262f159f6');
+    })
+})
