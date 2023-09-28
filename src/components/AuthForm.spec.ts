cb3dcdde23722e465e5705627d23ecd25e2b9608
M-Shrief <m.shrief.dev@proton.me>
2023-09-28 21:26:24 +0300

    added component testing with Vitest for AuthForm

---
+++ src/components/AuthForm.spec.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it, vi } from 'vitest'
+import { mount, flushPromises } from '@vue/test-utils';
+import waitForExpect from 'wait-for-expect';
+// Components
+import AuthForm from './AuthForm.vue';
+// Stores
+import { actions as partnerActions} from '../stores/partners';
+
+describe('<AuthForm />',  async () => {
+    it('Login', async() => {
+        const loginSpy = vi.spyOn(partnerActions, "login")
+        const wrapper = mount(AuthForm);
+
+        await wrapper.find('#phone').setValue('01235554567');
+        await wrapper.find('#password').setValue('P@ssword1');
+
+        await wrapper.find('button[type=submit]').trigger('submit');
+        await flushPromises();
+
+        await waitForExpect(() => {
+            expect(loginSpy).toHaveBeenCalled();
+            expect(loginSpy).toHaveBeenCalledWith({phone: '01235554567', password: 'P@ssword1'});
+        })
+    })
+
+    it('Signup', async () => {
+        const signup = vi.spyOn(partnerActions, 'signup');
+        const wrapper = mount(AuthForm);
+        
+        await wrapper.find('#toggle').trigger('click')
+
+        await wrapper.find('#name').setValue('new User')
+        await wrapper.find('#phone').setValue('01235554567');
+        await wrapper.find('#password').setValue('P@ssword1');
+        await wrapper.find('button[type=submit]').trigger('submit')
+        await flushPromises();
+
+        await waitForExpect(() => {
+            expect(signup).toHaveBeenCalled()
+            expect(signup).toHaveBeenCalledWith({
+                name: 'new User',
+                phone: '01235554567',
+                password: 'P@ssword1'
+            });
+        })
+
+    })
+})
