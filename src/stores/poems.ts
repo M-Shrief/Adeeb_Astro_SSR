1b6c1a746606981703b5bf0620f914a265700ec0
M-Shrief <m.shrief.dev@proton.me>
2023-12-30 17:09:31 +0200

    Added more checks to prevent duplicated request fetching the same data, restructured code to combine related blocks near each other.

--- src/stores/poems.ts
+++ src/stores/poems.ts
@@ -11,6 +11,8 @@ const poems = shallowRef<Poem[]>([]);
 export const getPoems = computed<Poem[]>(() => { return poems.value });
 
 export async function fetchPoems() {
+  if(poems.value.length != 0) return
+  
   const res = await fetch(
     apiURL(`/poems_intros`), 
     {
@@ -29,18 +31,9 @@ const poem = shallowRef<Poem>({} as Poem);
 
 export const getPoem = computed<Poem>(() => { return poem.value}); 
 
-const otherPoems = shallowRef<Poem[]>([]);
-export const getOtherPoems = computed<Poem[]>(() => { return otherPoems.value});
-
-export async function fetchOtherPoems(id: string) {
-  if(getPoems.value.length === 0) await fetchPoems();
-  const poemsArr = [...getPoems.value];
-  let poemIndex = poemsArr.map((poem: Poem) => poem.id).indexOf(id);
-  poemsArr.splice(poemIndex, 1);
-  otherPoems.value = poemsArr;
-}
-
 export async function fetchPoem(id: string) {
+  if(poem.value.id == id) return
+
   const res = await fetch(
     apiURL(`/poem/${id}`), 
     {
@@ -54,4 +47,15 @@ export async function fetchPoem(id: string) {
   } else {
     useFetchError(await res.json())
   }
+}
+
+const otherPoems = shallowRef<Poem[]>([]);
+export const getOtherPoems = computed<Poem[]>(() => { return otherPoems.value});
+
+export async function fetchOtherPoems(id: string) {
+  if(poems.value.length === 0) await fetchPoems();
+  const poemsArr = [...getPoems.value];
+  let poemIndex = poemsArr.map((poem: Poem) => poem.id).indexOf(id);
+  poemsArr.splice(poemIndex, 1);
+  otherPoems.value = poemsArr;
 }
