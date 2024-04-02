d46921e81d4bd88d7498fc8c974b1e01428d2f35
M-Shrief <m.shrief.dev@proton.me>
2024-04-02 14:53:40 +0200

    Added getI18nPrefix for i18n utils

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,33 @@
-import { ui, defaultLang } from './ui';
+import { languages, defaultLang } from "./langs";
 
+
+/**
+ * Get i18n lang from url
+ * @param url 
+ * @returns 
+ * 
+ * @example
+ * ```astro
+ * ---
+ * const currentLocale = getLangFromUrl(Astro.url);
+ * ---
+ * ```
+ */
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
-  if (lang in ui) return lang as keyof typeof ui;
+  const [, lang] = url.pathname.split("/");
+  if (lang in languages) return lang as keyof typeof languages;
   return defaultLang;
 }
+
+/**
+ * Used to compute i18n prefix.
+ * @param currentLocale 
+ * @returns 
+ * 
+ * @example
+ * ```astro
+ * <a href=`${getI18nPrefix}/blog` />
+ * ```
+ */
+export const getI18nPrefix = (currentLocale: keyof typeof languages) =>
+  currentLocale == defaultLang ? "" : `/${currentLocale}`;
