ade624e62263c49d0e4fc5189baac18226be7fef
M-Shrief <m.shrief.dev@proton.me>
2024-04-02 14:18:19 +0200

    Extracted i18n themes and langs into its own module

---
+++ src/i18n/langs.ts
@@ -0,0 +1,13 @@
+
+/**
+ * I18n languages
+ */
+export const languages = {
+  ar: "العربية",
+  en: "English",
+};
+
+/**
+ * I18n default langs
+ */
+export const defaultLang = "ar";
---
+++ src/i18n/themes.ts
@@ -0,0 +1,14 @@
+/** 
+ * i18n Dark/Light themes
+ * 
+ * */ 
+export const themes = {
+  ar: {
+    light: "نهاري",
+    dark: "ليلي",
+  },
+  en: {
+    light: "Light",
+    dark: "Dark",
+  },
+};
